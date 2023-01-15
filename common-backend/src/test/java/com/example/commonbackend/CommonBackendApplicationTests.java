61f3336b1f0815329313d341f3de5a0e2505faa6
Sara <saeraa@gmail.com>
2023-01-15 13:40:23 +0100

    api changes

--- common-backend/src/test/java/com/example/commonbackend/CommonBackendApplicationTests.java
+++ common-backend/src/test/java/com/example/commonbackend/CommonBackendApplicationTests.java
@@ -5,27 +5,49 @@ import com.example.commonbackend.model.Order;
 import com.example.commonbackend.model.ProductDetailed;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @AutoConfigureMockMvc(printOnlyOnFailure = false)
 class CommonBackendApplicationTests {
+
+	@BeforeEach
+	public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+				.apply(documentationConfiguration(restDocumentation))
+				.alwaysDo(document("{method-name}", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())))
+				.build();
+	}
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -51,7 +73,25 @@ class CommonBackendApplicationTests {
 				.content("{\n" +
 						"    \"customerID\": \"VINET\"\n" +
 						"}"))
-				.andExpect(status().isCreated());
+				.andExpect(status().isCreated())
+				.andDo(document("create-order-example",
+						requestFields(fieldWithPath("customerID")
+								.description("The customer's ID."),
+								fieldWithPath("id").optional().type(JsonFieldType.STRING)
+										.description("The id of the order."),
+								fieldWithPath("orderDate").optional().type(JsonFieldType.STRING)
+										.description("The order date."),
+								fieldWithPath("shippedDate").optional().type(JsonFieldType.STRING)
+										.description("The shipping date.")),
+						responseFields(fieldWithPath("customerID")
+								.description("The customer's ID"),
+								fieldWithPath("id")
+										.description("Order ID"),
+								fieldWithPath("orderDate")
+								.description("Order date of order."),
+								fieldWithPath("shippedDate")
+										.description("Shipped date of order."))
+				));
 	}
 
 	@Test
